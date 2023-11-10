03ec018fa3e5f44df88d8cf798567ee4efbe2be1
alicializ <xlz200806@live.cn>
2023-11-10 14:23:57 +0800

    fix potential input leak

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ResourceLeakExample {
-   public static void main(String[] args) {
+/*    public static void main(String[] args) {
        BufferedReader reader = null;
        try {
            reader = new BufferedReader(new FileReader("test.txt"));
@@ -19,19 +19,19 @@ public class ResourceLeakExample {
            e.printStackTrace();
        }
        // Resource leak: 'reader' is never closed on normal or exceptional execution paths
+   } */
+    
+    
+   public static void main(String[] args) {
+       // 使用try-with-resources确保BufferedReader正常关闭
+       try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+           String firstLine = reader.readLine();
+           System.out.println(firstLine);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       // 不需要显式关闭BufferedReader，try-with-resources会自动处理
    }
-    
-    
-//    public static void main(String[] args) {
-//        // 使用try-with-resources确保BufferedReader正常关闭
-//        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
-//            String firstLine = reader.readLine();
-//            System.out.println(firstLine);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // 不需要显式关闭BufferedReader，try-with-resources会自动处理
-//    }
 	
 	// public static void main(String[] args) {
 		// String password;
